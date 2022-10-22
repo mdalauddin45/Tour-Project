d17a91a9ba9f940fff587cc69f0002b43f7d8503
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-22 17:31:08 +0600

    vsd

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Rectangle from "../assets/Rectangle 1.png";
+
+const Home = () => {
+  const myStyle = {
+    backgroundImage: url(${Rectangle}),
+    height: "100vh",
+    marginTop: "-70px",
+    fontSize: "50px",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  };
+  return (
+    <div style={myStyle}>
+      <h1>home</h1>
+      nsdfhklh mskfh
+    </div>
+  );
+};
+
+export default Home;
