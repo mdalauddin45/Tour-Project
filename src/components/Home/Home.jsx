89f72521a7ca230f926ea402f456c72bef1169a3
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-22 20:13:25 +0600

    all

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,37 @@
 import React from "react";
-import Rectangle from "../assets/Rectangle 1.png";
+import "./Home.css";
+import Rectangle from "../Image/Rectangle 1.png";
+import Sreemongol from "../Image/Sreemongol.png";
+import sundarbans from "../Image/sundorbon.png";
 
 const Home = () => {
-  const myStyle = {
-    backgroundImage: url(${Rectangle}),
-    height: "100vh",
-    marginTop: "-70px",
-    fontSize: "50px",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-  };
   return (
-    <div style={myStyle}>
-      <h1>home</h1>
-      nsdfhklh mskfh
+    <div className="home">
+      <div>
+        <h1>Cox's Bazar</h1>
+      </div>
+      <div>
+        <div class="slide-container">
+          <span class="slider-span" id="slider-span1"></span>
+          <span class="slider-span" id="slider-span2"></span>
+          <span class="slider-span" id="slider-span3"></span>
+
+          <div class="image-slider">
+            <div class="slides-div" id="slide-1">
+              <img src={Rectangle} alt="" class="img" id="img1" />
+              <a href="#slider-span1" class="button" id="button-1"></a>
+            </div>
+            <div class="slides-div" id="slide-2">
+              <img src={Sreemongol} alt="" class="img" id="img2" />
+              <a href="#slider-span2" class="button" id="button-2"></a>
+            </div>
+            <div class="slides-div" id="slide-3">
+              <img src={sundarbans} alt="" class="img" id="img3" />
+              <a href="#slider-span3" class="button" id="button-3"></a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
